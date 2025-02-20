200fc7122fade7977c63afd3408a1cb0e2ecf1b7
Josemar Ferreira <jf.sorin@gmail.com>
2025-02-20 11:43:54 -0300

    feat: aula 3

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -15,6 +17,9 @@ const SubscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof SubscriptionSchema>
 
 export function SubscriptionForm() {
+  const route = useRouter()
+  const searchParams = useSearchParams()
+
   const {
     register,
     handleSubmit,
@@ -23,8 +28,12 @@ export function SubscriptionForm() {
     resolver: zodResolver(SubscriptionSchema),
   })
 
-  function onSubscribe(data: SubscriptionSchema) {
-    console.log(data)
+  async function onSubscribe({ name, email }: SubscriptionSchema) {
+    const referrer = searchParams.get('referrer')
+
+    const { subscriberId } = await subscribeToEvent({ name, email, referrer })
+
+    route.push(`/invite/${subscriberId}`)
   }
 
   return (
--- src/app/invite/[subscribeId]/page.tsx
+++ src/app/invite/[subscribeId]/page.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
 import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 
-export default function InvitePage() {
-  const inviteLink = 'http://localhost:3000/invite/3179271031212341'
+interface InvitePageProps {
+  params: Promise<{
+    subscribeId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscribeId } = await props.params
+  const inviteLink = `http://localhost:3333/invites/${subscribeId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -35,7 +42,7 @@ export default function InvitePage() {
 
           <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscribeId={subscribeId} />
         </div>
       </div>
 
